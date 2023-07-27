dcf25931330d8777383ff66f10d00e4a57fe5daf
ucr_qinye <953218204@qq.com>
2023-07-27 11:08:13 +0800

    fix: update build.sh

--- docs/.vuepress/realcugan-ncnn-webassembly/build.sh
+++ docs/.vuepress/realcugan-ncnn-webassembly/build.sh
@@ -6,8 +6,8 @@
 source ./emsdk/emsdk_env.sh
 
 target_dir="../../public/tools"
-mkdir -p ./build $target_dir
-rm -rf ./build/*
+mkdir -p ./build
+# rm -rf ./build/*
 (cd ./build && \
 cmake -DCMAKE_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DWASM_FEATURE=simd-threads -DNCNN_THREADS=ON -DNCNN_OPENMP=ON -DNCNN_SIMPLEOMP=ON -DNCNN_RUNTIME_CPU=OFF -DNCNN_SSE2=ON -DNCNN_AVX2=OFF -DNCNN_AVX=OFF -DNCNN_BUILD_TOOLS=OFF -DNCNN_BUILD_EXAMPLES=OFF -DNCNN_BUILD_BENCHMARK=OFF -DNCNN_TTT=ON .. && \
 make -j4 && \
